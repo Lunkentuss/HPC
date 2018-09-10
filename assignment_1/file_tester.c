54268e8f824174eb340bc3f959c2591d7da56007
Peter Hansson <peter__hansson@hotmail.com>
2018-09-10 13:54:00 +0200

    Updated

---
+++ assignment_1/file_tester.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "matrix.h"
+
+// Creates a matrix struct with entries like a multiplication table specified
+// by size.
+struct matrix * multiplication_table(int size) {
+    struct matrix * mat = matrix_init(size, size);
+    for (int i = 1 ; i <= mat->m ; i++)
+        for (int j = 1 ; j <= mat->n ; j++)
+            mat->as[i-1][j-1] = i * j;
+
+    return(mat);
+}
+
+int main() {
+    FILE * file;
+    char * file_name = "matrix.dat";
+
+    // Open and write to file
+    file = fopen(file_name, "w");
+    int size = 10;
+    struct matrix * mat = multiplication_table(size);
+    write_matrix(file, mat);
+    fclose(file);
+
+    // Open and read file
+    file = fopen(file_name, "r");
+    struct matrix * r_mat = read_matrix(file);
+    fclose(file);
+
+    // Test if matrices is equal
+    bool is_equal = matrix_equal(mat, r_mat);
+    if (is_equal == true) {
+        printf("The matrices is equal\n");
+    }
+    else {
+        printf("The matrices is not equal\n");
+    }
+
+    return(0);
+}
---
+++ assignment_1/matrix.h
@@ -0,0 +1,88 @@
+#include <stdbool.h>
+
+struct matrix{
+    int * asentries;
+    int ** as;
+    int m;
+    int n;
+};
+
+struct matrix * matrix_init(int m, int n)
+{
+    struct matrix * mat = (struct matrix *)malloc(sizeof(struct matrix));
+    int * asentries = (int *)malloc(sizeof(int) * m * n);
+    int ** as = (int **)malloc(sizeof(int *) * m);
+
+    for(int ix = 0, jx = 0 ; ix < m ; ix++, jx+=n)
+        as[ix] = asentries + jx;
+
+    mat->as = as;
+    mat->asentries = asentries;
+    mat->m = m;
+    mat->n = n;
+
+    return(mat);
+};
+
+bool matrix_equal(struct matrix * mat1, struct matrix * mat2) {
+    if (mat1->m != mat2->m) {
+        return(false);
+    }
+
+    if (mat1->n != mat2->n) {
+        return(false);
+    }
+
+    for (int i = 0 ; i < mat1->m ; i++) {
+        for (int j = 0 ; j < mat2->n ; j++) {
+            if (mat1->as[i][j] != mat2->as[i][j]){
+                return(false);
+            }
+        }
+    }
+
+    return(true);
+}
+
+void matrix_print(struct matrix * mat) {
+    for (int i = 0 ; i < mat->m ; i++) {
+        for (int j = 0 ; j < mat->n ; j++) {
+            printf("%d ", mat->as[i][j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+
+void matrix_free(struct matrix * mat){
+    free(mat->asentries);
+    free(mat->as);
+
+    return;
+}
+
+void write_matrix(FILE * fp, struct matrix * mat)
+{
+    fwrite(&mat->m, sizeof(int), 1, fp);
+    fwrite(&mat->n, sizeof(int), 1, fp);
+
+    for (int i = 0 ; i < mat->m ; i++) {
+        for (int j = 0 ; j < mat->n ; j++) {
+            fwrite(&mat->as[i][j], sizeof(int), 1, fp);
+        }
+    }
+    return;
+}
+
+struct matrix * read_matrix(FILE * fp)
+{
+    int m, n;
+
+    fread(&m, sizeof(int), 1, fp);
+    fread(&n, sizeof(int), 1, fp);
+
+    struct matrix * mat = matrix_init(m, n);
+    fread(*mat->as, sizeof(int), m * n, fp);
+
+    return(mat);
+}
