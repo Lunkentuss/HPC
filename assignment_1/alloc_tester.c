455bc5b9f02974b475beba16415bdb2849972964
Peter Hansson <peter__hansson@hotmail.com>
2018-09-10 15:59:44 +0200

    Added files

---
+++ assignment_1/alloc_tester.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "matrix.h"
+
+#define SIZE 30000
+
+void allocate_array_stack()
+{
+    int as[SIZE];
+    for(size_t ix=0 ; ix < SIZE ; ix++)
+        as[ix] = 0;
+
+    printf("First array number: %d\n", as[0]);
+    return;
+}
+
+void allocate_array_heap()
+{
+    int * as = (int *)malloc(sizeof(int) * SIZE);
+    for(size_t ix=0 ; ix < SIZE ; ix++)
+        as[ix] = 0;
+
+    printf("First array number: %d\n", as[0]);
+    free(as);
+    return;
+}
+
+void fragmentation_avoid_false()
+{
+    int ** as = (int **)malloc(sizeof(int *) * SIZE);
+    for(size_t ix = 0 ; ix < SIZE ; ix++)
+        as[ix] = (int *)malloc(sizeof(int) * SIZE);
+
+    for(size_t ix = 0 ; ix < SIZE ; ix++)
+        for(size_t jx = 0 ; jx < SIZE ; jx++)
+            as[ix][jx] = 0;
+
+    printf("Matrix[0][0]: %d\n", as[0][0]);
+
+    for(size_t ix = 0 ; ix < SIZE ; ix++)
+        free(as[ix]);
+
+    return;
+}
+
+void fragmentation_avoid_true()
+{
+    int * asentries = (int *)malloc(sizeof(int) * SIZE * SIZE);
+    int ** as = (int **)malloc(sizeof(int *) * SIZE);
+
+    for(int ix = 0, jx = 0 ; ix < SIZE ; ix++, jx += SIZE)
+        as[ix] = asentries + jx;
+
+    for(int ix = 0 ; ix < SIZE ; ix++)
+        for(int jx = 0 ; jx < SIZE ; jx++)
+            as[ix][jx] = 0;
+
+    printf("Matrix[0][0]: %d\n", as[0][0]);
+
+    free(asentries);
+    free(as);
+
+    return;
+}
+
+int main(int argc, char * argv[]) {
+    allocate_array_stack();
+    allocate_array_heap();
+    fragmentation_avoid_false();
+    fragmentation_avoid_true();
+    struct matrix * mat = matrix_init(5,10);
+    for(int ix = 0 ; ix < mat->m ; ix++)
+        for(int jx = 0 ; jx < mat->n ; jx++)
+            mat->as[ix][jx] = ix * jx;
+
+    printf("\n");
+    return(0);
+}
---
+++ assignment_1/printargs_tester.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ARG_COUNT 5
+#define BASE 10
+
+void run_program(char * argv[]) {
+    long a, b;
+
+    // Check if flags are equal
+    if (strcmp(argv[1], argv[3]) == 0) {
+        printf("Equal flags are not allowed.\n"); 
+        return;
+    }
+
+    int i = 1;
+    while (i < ARG_COUNT) {
+        char * flag = argv[i];
+        char * value = argv[i+1];
+        char * end_ptr;
+        long value_l = strtol(value, &end_ptr, BASE);
+
+        if (strcmp(flag, "-a") == 0) {
+            a = value_l;
+        }
+        else if (strcmp(flag, "-b") == 0) {
+            b = value_l;
+        }
+        else {
+            printf("Flag %s is not legit.\n", flag);
+            return;
+        }
+
+        i = i + 2;
+    }
+    printf("A is %d, B is %d\n", a, b);
+
+    return;
+}
+
+int main(int argc, char * argv[])
+{
+    if (argc == ARG_COUNT) {
+        run_program(argv);
+    }
+    else{
+        printf("The number of arguments should be equal to: %d\n",
+               ARG_COUNT - 1);
+    }
+
+    return(0);
+}
