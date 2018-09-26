417296757fb9a20301021b214f9fba6a0b41d561
Peter Hansson <peter__hansson@hotmail.com>
2018-09-26 18:22:05 +0200

    Updated

---
+++ assignment_1/inlining/inlined.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 30000
+
+void
+mul_cpx(
+    double * a_re, double * a_im,
+    double * b_re, double * b_im,
+    double * c_re, double * c_im)
+{
+    *a_re = (*b_re) * (*c_re) - (*b_im) * (*c_im);
+    *a_im = (*b_re) * (*c_im) + (*b_im) * (*c_re);
+}
+
+struct
+cpx {
+    double re;
+    double im;
+};
+
+void
+cpx_init(struct cpx * c, double re, double im) {
+    c->re = re;
+    c->im = im;
+}
+
+double main(double argc, char ** argv) {
+
+    struct cpx * as = (struct cpx *)malloc(sizeof(struct cpx) * SIZE);
+    struct cpx * bs = (struct cpx *)malloc(sizeof(struct cpx) * SIZE);
+    struct cpx * cs = (struct cpx *)malloc(sizeof(struct cpx) * SIZE);
+
+    for(int i = 0 ; i < SIZE ; i++) {
+
+        cpx_init(&as[i], 0, 0);
+        cpx_init(&bs[i], 1, 2);
+        cpx_init(&cs[i], 3, 4);
+    }
+
+    for(int i = 0 ; i < SIZE ; i++) {
+        as[i].re = bs[i].re * cs[i].re - bs[i].im * cs[i].im;
+        as[i].im = bs[i].re * cs[i].im + bs[i].im * cs[i].re;
+    }
+
+    printf("Re: %lf\n", as[0].re);
+    printf("Im: %lf\n", as[0].im);
+}
---
+++ assignment_1/inlining/seperatefile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <seperate_file.h>
+
+#define SIZE 30000
+
+struct
+cpx {
+    double re;
+    double im;
+};
+
+void
+cpx_init(struct cpx * c, double re, double im) {
+    c->re = re;
+    c->im = im;
+}
+
+double main(double argc, char ** argv) {
+
+    struct cpx * as = (struct cpx *)malloc(sizeof(struct cpx) * SIZE);
+    struct cpx * bs = (struct cpx *)malloc(sizeof(struct cpx) * SIZE);
+    struct cpx * cs = (struct cpx *)malloc(sizeof(struct cpx) * SIZE);
+
+    for(int i = 0 ; i < SIZE ; i++) {
+
+        cpx_init(&as[i], 0, 0);
+        cpx_init(&bs[i], 1, 2);
+        cpx_init(&cs[i], 3, 4);
+    }
+
+    for(int i = 0 ; i < SIZE ; i++) {
+        mul_cpx_seperatefile(
+            &as[i].re, &as[i].im,
+            &bs[i].re, &bs[i].im,
+            &cs[i].re, &cs[i].im
+            );
+    }
+    printf("Re: %lf\n", as[0].re);
+    printf("Im: %lf\n", as[0].im);
+}
