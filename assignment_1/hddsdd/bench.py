0b80a10751d2d9967499b63116f1913772f4dd94
Lunkentus <peter__hansson@hotmail.com>
2018-09-27 14:04:59 +0200

    Updated

---
+++ assignment_1/hddsdd/bench.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+import subprocess as sub
+import time
+import sys
+
+if len(sys.argv) < 2:
+    print("Forgot number of iterations")
+    exit()
+
+n = int(sys.argv[1])
+
+
+start = time.time()
+
+for i in range(n):
+    sub.call(["./main_write.c"])
+
+elapsed = time.time() - start
+
+print("Time " + "write" + ":", elapsed)
+
+print("===============")
+
+for i in range(n):
+    sub.call(["./main_read.c"])
+
+elapsed = time.time() - start
+
+print("Time " + "read" + ":", elapsed)
