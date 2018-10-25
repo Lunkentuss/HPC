5c85b00e16d7744e6a37de54bf2ea235b91d55a3
Lunkentus <peter__hansson@hotmail.com>
2018-10-25 10:32:21 +0200

    Updted

--- assignment_3/util/fixed_point.h
+++ assignment_3/util/fixed_point.h
@@ -1,11 +1,16 @@
 #ifndef H_FIXED_POINT
 # define H_FIXED_POINT
 
-#define FIXED_FACT 1000
-#define FIXED_MULTIPLY(x, y) (int)((x) * (y) / FIXED_FACT)
-#define INT_TO_FIXED(x) x * FIXED_FACT
-#define FIXED_TO_INT(x) (int)(x / FIXED_FACT)
-#define FLOAT_TO_FIXED(x) (int)(x * FIXED_FACT)
-#define FIXED_TO_FLOAT(x) (float)((x) / FIXED_FACT)
+#define DECI_FACT 1000
+#define FIXED_POS 6
+#define FIXED_MULTIPLY(x, y) ((x) * (y) >> FIXED_POS)
+#define INT_TO_FIXED(x) ((x) << FIXED_POS)
+#define FIXED_TO_INT(x) ((x) >> FIXED_POS)
+#define FIXED_TO_FLOAT(x) (float)((x) >> FIXED_FACT)
+
+static inline int
+fixdeci_to_fixbin(const int deci, int sign)
+{
+}
 
 #endif
